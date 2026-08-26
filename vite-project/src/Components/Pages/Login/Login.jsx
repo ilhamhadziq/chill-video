17daf5acaf088abcaf3e6a016ad679e8e012978e
ilhamhadziq <ilhamhadziq24@gmail.com>
2026-08-27 04:02:20 +0700

    css form

--- vite-project/src/Components/Pages/Login/Login.jsx
+++ vite-project/src/Components/Pages/Login/Login.jsx
@@ -2,10 +2,10 @@ import Button from "../../UI/Button";
 const Login = ()=> {
     return(
         <main className="min-h-screen bg-[url('public/background.jpg')] bg-center bg-cover flex items-center justify-center text-white">
-            <form action="submit" className="flex flex-col items-center justify-center gap-5 p-6 bg-[#181A1CD6] opacity-85 rounded-md">
+            <form action="submit" className="flex flex-col items-center justify-center gap-5 p-6 bg-[#181A1CD6] opacity-85 rounded-lg min-h-96">
                 <h1>Masuk</h1>
                 <p>Selamat Datang Kembali</p>
-                <Button>Masuk</Button>
+                <Button className={'w-full'}>Masuk</Button>
             </form>                        
         </main>
     )
