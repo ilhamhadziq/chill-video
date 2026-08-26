5e913a97279c1c9e266459a561fb9ba9e3b19fdb
ilhamhadziq <ilhamhadziq24@gmail.com>
2026-08-26 17:41:59 +0700

    feat: update package route

--- vite-project/src/Components/Pages/Login/Login.jsx
+++ vite-project/src/Components/Pages/Login/Login.jsx
@@ -1,7 +1,10 @@
+import Button from "../../UI/Button";
 const Login = ()=> {
     return(
-        <main>
-
+        <main className="bg-gray-400 min-h-screen text-white">
+            <form action="submit">
+                
+            </form>                        
         </main>
     )
 }
