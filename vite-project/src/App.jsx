d35406faf7d2439c5d52ea484b3353cfcac99e6f
ilhamhadziq <ilhamhadziq24@gmail.com>
2026-08-26 17:17:07 +0700

    feat: add tailwind package

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,9 @@
 import './index.css'
 function App() {
   return (
-    <>
+    <main className='bg-violet-600 min-h-screen'>
       <h1 className="text-3xl to-blue-400 flex justify-center">Coba Tailwind</h1>
-    </>
+    </main>
   )
 }
 
