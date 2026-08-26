8dadf7741289452f260b674cccf2a61259018e5b
ilhamhadziq <ilhamhadziq24@gmail.com>
2026-08-26 20:08:27 +0700

    Login

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,10 @@
 import './index.css'
-import Button from './Components/UI/Button'
+import Login from './Components/Pages/Login/Login'
 function App() {
   return (
-    <main className='bg-gray-400  min-h-screen flex flex-col justify-center items-center'>
-      <h1 className="text-3xl to-blue-400 flex justify-center">Coba Tailwind</h1>
-      <Button>Masuk</Button>
-    </main>
+    <>
+      <Login></Login>
+    </>
   )
 }
 
--- vite-project/src/Components/Pages/Login/Login.jsx
+++ vite-project/src/Components/Pages/Login/Login.jsx
@@ -1,9 +1,10 @@
 import Button from "../../UI/Button";
 const Login = ()=> {
     return(
-        <main className="bg-gray-400 min-h-screen text-white">
-            <form action="submit">
-                
+        <main className="flex justify-center items-center text-white bg-[#181A1CD6] opacity-85">
+            <form action="submit" className="rounded-md bg-[#181A1CD6] opacity-85 ">
+                <h1>Masuk</h1>
+                <Button>Masuk</Button>
             </form>                        
         </main>
     )
