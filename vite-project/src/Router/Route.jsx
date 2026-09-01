69724b4a318981957ba06ebe6a6da1c139bc3141
ilhamhadziq <ilhamhadziq24@gmail.com>
2026-09-01 17:40:15 +0700

    add: react-dom

---
+++ vite-project/src/Router/Route.jsx
@@ -0,0 +1,18 @@
+import Home from '../Components/Pages/Home'
+import Login from '../Components/Pages/Login'
+import Register from '../Components/Pages/Register'
+const Routes = [
+    {
+        path:'/',
+        element: (<Home/>)
+    },
+    {
+        path:'/',
+        element: (<Login/>)
+    },
+    {
+        path:'/',
+        element: (<Register/>)
+    },
+    
+]
