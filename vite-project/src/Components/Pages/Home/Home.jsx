576d4dc1dbc42f7c7223b8a21a430da20ec35b1c
ilhamhadziq <ilhamhadziq24@gmail.com>
2026-09-03 21:05:45 +0700

    add card to home

--- vite-project/src/Components/Pages/Home/Home.jsx
+++ vite-project/src/Components/Pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import Header from "../../Layout/Header";
+import Card from "../../UI/Card";
 
 const Home = ()=> {
     return(
         <main className="min-h-screen bg-black">
             <Header></Header>
+            <br className="mb-4"/>
+            <Card src="public/allofus.png" alt="All of Us"></Card>
         </main>
     )
 }
--- vite-project/src/Components/UI/Card/Card.jsx
+++ vite-project/src/Components/UI/Card/Card.jsx
@@ -1,7 +1,10 @@
-const Card = ()=>{
+const Card = (props)=>{
+    const {src, alt}=props
     return(
-        <section>
-
+        <section  className="relative">
+            <img src={src} alt={alt} className="w-23.75 h-auto relative sm:w-50" />
+            <button className="absolute left-[10%] top-[15%] rounded-xl bg-[#0F1E93]">episode terbaru</button>
+            <h1 className="absolute top-0 right-0 bg-[#B71F1D] p-1">Top 10</h1>
         </section>
     )
 }
