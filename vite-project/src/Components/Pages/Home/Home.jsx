6f4e22d13fa4772c5e8badbb599bbab452045f86
ilhamhadziq <ilhamhadziq24@gmail.com>
2026-09-05 12:07:20 +0700

    update home

--- vite-project/src/Components/Pages/Home/Home.jsx
+++ vite-project/src/Components/Pages/Home/Home.jsx
@@ -3,10 +3,16 @@ import Card from "../../UI/Card";
 
 const Home = ()=> {
     return(
-        <main className="min-h-screen bg-black">
+        <main className="min-h-screen bg-black ">
             <Header></Header>
             <br className="mb-4"/>
-            <Card src="public/allofus.png" alt="All of Us"></Card>
+            <section className="grid grid-cols-3 gap-3 py-1.5 px-5 items-center justify-center sm:grid-cols-6">
+                <Card src="public/allofus.png" alt="All of Us"></Card>
+                <Card src="public/allofus.png" alt="All of Us"></Card>
+                <Card src="public/allofus.png" alt="All of Us"></Card>
+                <Card src="public/allofus.png" alt="All of Us"></Card>
+            </section>
+            
         </main>
     )
 }
--- vite-project/src/Components/UI/Card/Card.jsx
+++ vite-project/src/Components/UI/Card/Card.jsx
@@ -1,10 +1,14 @@
 const Card = (props)=>{
     const {src, alt}=props
     return(
-        <section  className="relative w-23.75 sm:w-50 text-white">
+        <section  className="relative w-23.75 sm:w-50 text-white rounded-md text-[6px] sm:text-[14px] font bold">
             <img src={src} alt={alt} className="w-full h-auto relative " />
             <button className="absolute left-[5%] top-[2%] rounded-xl bg-[#0F1E93] p-2">episode terbaru</button>
-            <h1 className="absolute top-0 right-0 bg-[#B71F1D] p-1">Top 10</h1>
+            {/* <h1 className="absolute top-0 right-0 bg-[#B71F1D] p-1 w-">Top 10</h1> */}
+            <div className="absolute top-0 right-0 flex flex-col items-center leading-none font-normal bg-[#B71F1D] p-1 gap-1">
+                <span>TOP</span>
+                <span>10</span>
+            </div>
         </section>
     )
 }
